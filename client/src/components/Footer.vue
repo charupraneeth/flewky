91d09ecdbae703815737eb0e0f1f416212f4260a
charupraneeth <charupraneeth@gmail.com>
2023-03-19 12:18:12 +0530

    Update: Footer

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -2,12 +2,12 @@
   <footer>
     <small>
       Contact us at
-      <a class="mail" href="mailto:support@flewky.com"
-        >support@flewky.com</a
+      <a class="mail" href="mailto:mwpfrgw8@anonaddy.me"
+        >mwpfrgw8@anonaddy.me</a
       ></small
     >
     <br />
-    <h5>Flewky.com © 2021</h5>
+    <!-- <h5>Flewky.com © 2021</h5> -->
   </footer>
 </template>
 <style lang="scss" scoped>
